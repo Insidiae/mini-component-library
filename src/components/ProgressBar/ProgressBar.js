58b0e29ddff40cc60d78076088bffd95925abcbb
Kobe Ruado <insidiae423@gmail.com>
2022-03-13 00:11:45 +0800

    Add ProgressBar component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,80 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const STYLES = {
+  small: {
+    height: 8,
+    padding: 0,
+    radius: 4,
+  },
+  medium: {
+    height: 12,
+    padding: 0,
+    radius: 4,
+  },
+  large: {
+    height: 16,
+    padding: 4,
+    radius: 8,
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+  }
+
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        "--padding": styles.padding + "px",
+        "--radius": styles.radius + "px",
+      }}
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      {/* <strong>{value}%</strong> */}
+      <Trimmer
+        style={{
+          "--width": value + "%",
+          "--height": styles.height + "px",
+        }}
+      >
+        <Bar value={value} />
+      </Trimmer>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  /* width: 185px; */
+  padding: var(--padding);
+  border-radius: var(--radius);
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const Trimmer = styled.div`
+  border-radius: 4px;
+
+  /* Trim off corners when progress bar ain't full */
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+`;
 
 export default ProgressBar;
